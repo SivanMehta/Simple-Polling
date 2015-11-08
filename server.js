36f679e5c9f212c30690c9c8afdb69aa9eb8696e
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-11-08 17:31:10 -0500

    added ejs acomodation

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ var express = require("express");
 var morgan = require('morgan');
 
 var app = express();
+app.set('view engine', 'ejs');
 app.use(morgan('tiny'));
 
 // load the routes
 require('./routes/poll.js').init(app);
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
 
 app.listen(50000);
 
