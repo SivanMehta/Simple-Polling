b813e6312d758bff84be40b9576b427fe3b5ebcd
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-11-09 13:03:10 -0500

    added ability to vote

--- views/js/page.js
+++ views/js/page.js
@@ -4,7 +4,18 @@ $(function()
     (
         function()
         {
-            console.log("here!");
+            var poll_name = $("h1").text();
+
+            var choice = window.prompt("What is your choice?");
+
+            $.ajax({
+                url: '/polls/' + poll_name + "/vote/" + choice,
+                type: "POST",
+                success: function(result)
+                {
+                    location.reload();
+                }
+            });
         }
     )
 });
